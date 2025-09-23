90d1671601c4077a1b60dd6cbfb87993d8ea2c54
jhustyn <111186210+jhustinn@users.noreply.github.com>
2025-09-23 11:48:15 +0700

    Approve PI Finnance

--- src/data/menuConfig.ts
+++ src/data/menuConfig.ts
@@ -1305,6 +1305,12 @@ export const financeMenu: MenuSection[] = [
   //   ],
   // },
   {
+    title: "Approve PI",
+    icon: "CheckCircle",
+    items: [],
+    directPath: "/finance/approve-pi",
+  },
+  {
     title: "Voucher",
     icon: "Ticket",
     items: [
